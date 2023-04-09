4fbe89df8a48299f6bbfed64223b799df6c3b126
lomasnz <paul.armstrong@section6.io>
2023-04-09 14:58:38 +1200

    deleted:    create_sym_link.sh

--- create_sym_link.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Your shell commands here
-find . -type f -not -path './.git/*' -not -name 'create_sym_link.sh' -print0 | while IFS= read -r -d '' file; do ln -s "$(realpath "$file")" /Users/armstrongp/Documents/coding/gas-warpdrive6/"$(basename "$file")"; done
