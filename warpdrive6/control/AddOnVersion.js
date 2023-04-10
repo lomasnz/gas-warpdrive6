a12dba6c3a753445bf67c18d23776c184cfbdd6a
lomasnz <paul.armstrong@section6.io>
2023-04-10 21:57:12 +1200

    2.1.2, 10 April 2023

--- warpdrive6/control/AddOnVersion.js
+++ warpdrive6/control/AddOnVersion.js
@@ -3,7 +3,7 @@ const ADD_ON_VERSION_FIELD = "version";
 const ADD_ON_NAME = "S6 TOOL BOX";
 const ADD_ON_METADATA_VERSION = "1.1";
 const ADD_ON_METADATA_VERSION_2 = "2.0";
-const ADD_ON_RELEASE = "2.1.1, 10 April 2023";
+const ADD_ON_RELEASE = "2.1.2, 10 April 2023";
 
 const META_DATA = {
   VALUE_FROM_FOLDER: "YES",
